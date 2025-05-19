a6d40a0da96aa5560eb8948f0b5958218cf1cab0
JavaMetalCoder <zagariafabio1999@gmail.com>
2025-05-19 22:59:57 +0200

    feat: added es4

---
+++ html-css-js/es4/ecmascript.js
@@ -0,0 +1,51 @@
+/* es1 */
+
+function filtraPari(array) {
+  let newArray = [];
+  if (!Array.isArray(array)) {
+    throw new TypeError("Errore: parametro non è un array!");
+  } else {
+    console.log("Analizzo!");
+  }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      console.log("PARI!");
+      newArray.push(array[i]);
+      
+    } else {
+      console.log("DISPARI");
+       
+    }
+  }
+  return newArray;
+}
+
+console.log(filtraPari([1, 4, 6, 7]));
+
+/* es2 */
+
+function contaVocali(string) {
+  
+  //CHECK-IN
+  if(typeof(string) !== "string") {
+    throw new TypeError("Errore: string param non è una stringa!");
+
+  }
+
+  //variabili per le vocali
+  const vocali = "aeiou";
+  let i = 0; 
+
+  for (let char of string.toLowerCase()) {
+    if (vocali.includes(char)) {
+      i++;
+
+    }
+
+  }
+  return i;
+
+}
+
+console.log(contaVocali("Perdincibacco"));
+
